2f7d201a1ed71efd0d24c721b3a40373343a8c8d
z.davlatov <z.davlatov@atmos.uz>
2023-05-26 19:05:03 +0500

    Project configuration

---
+++ src/test/java/com/marketplace/jingo/JingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.jingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
